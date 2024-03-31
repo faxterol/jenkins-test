328f71c523cc21352a23b555bd05854fc92844fa
Luis Carlos Cárdenas <carlos@faxterol.com>
2024-03-31 15:26:15 -0600

    test demo project

---
+++ src/test/java/com/faxterol/demojenkins/demojenkins/DemojenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faxterol.demojenkins.demojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
